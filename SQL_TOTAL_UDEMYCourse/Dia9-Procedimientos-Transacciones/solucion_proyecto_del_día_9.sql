9bb898a9f68f031a376360dadf653946d73b174c
Pablo Pombo <pablo-pombo@hotmail.com>
2026-04-27 23:34:02 -0600

    Adding gitignore

--- SQL_TOTAL_UDEMYCourse/Dia9-Procedimientos-Transacciones/solucion_proyecto_del_día_9.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER //
-
-CREATE PROCEDURE AgregarDepartamento(IN _nombre VARCHAR(255), IN _ubicacion VARCHAR(255))
-BEGIN
-	INSERT INTO Departamentos (nombre, ubicacion) VALUES (_nombre, _ubicacion);
-END //
-
-DELIMITER ;
